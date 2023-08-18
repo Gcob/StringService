7e449e5de379281a62f8202d3a0234c5538120ba
Jacob <jacobproulx@gmail.com>
2023-08-18 14:55:54 -0400

    Added stringservice with encode and remove accent methods. Plus added unittests

---
+++ src/StringService.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace gcob\StringService;
+
+use Transliterator;
+
+class StringService
+{
+    private static $instance;
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance(): self
+    {
+        if (null === static::$instance) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
+    public function encodeString(string $input): string
+    {
+        $output = $this->removeAccents($input);
+        $output = strtolower($output);
+        $output = trim($output, " \t\n\r");
+        $output= preg_replace('/\s+/', '-', $output);
+        $output = preg_replace('/[^a-z0-9\s\-]/', '', $output);
+
+        return $output;
+    }
+
+    function removeAccents(string $input): string
+    {
+        $accents = array(
+            'à' => 'a', 'â' => 'a', 'ä' => 'a', 'á' => 'a', 'ã' => 'a',
+            'é' => 'e', 'è' => 'e', 'ê' => 'e', 'ë' => 'e',
+            'î' => 'i', 'ï' => 'i', 'í' => 'i',
+            'ö' => 'o', 'ô' => 'o', 'ò' => 'o', 'ó' => 'o', 'õ' => 'o',
+            'ù' => 'u', 'û' => 'u', 'ü' => 'u', 'ú' => 'u',
+            'ç' => 'c', 'ñ' => 'n',
+
+            'À' => 'A', 'Â' => 'A', 'Ä' => 'A', 'Á' => 'A', 'Ã' => 'A',
+            'É' => 'E', 'È' => 'E', 'Ê' => 'E', 'Ë' => 'E',
+            'Î' => 'I', 'Ï' => 'I', 'Í' => 'I',
+            'Ö' => 'O', 'Ô' => 'O', 'Ò' => 'O', 'Ó' => 'O', 'Õ' => 'O',
+            'Ù' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ú' => 'U',
+            'Ç' => 'C', 'Ñ' => 'N'
+        );
+
+        return strtr($input, $accents);
+    }
+
+
+
+}
---
+++ tests/StringServiceTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use gcob\StringService\StringService;
+use PHPUnit\Framework\TestCase;
+
+class StringServiceTest extends TestCase
+{
+    public function testRemoveAccent()
+    {
+        $stringService = StringService::getInstance();
+
+        $output = $stringService->removeAccents("éÉàÀùÙçÇ");
+
+        $this->assertEquals("eEaAuUcC", $output);
+    }
+
+    public function testEncodeString()
+    {
+        $stringService = StringService::getInstance();
+        $input = "  Ma  -chaine##!   PÀS ENcodé";
+        $expectedOutput = "ma--chaine-pas-encode";
+
+        $output = $stringService->encodeString($input);
+
+        $this->assertEquals($expectedOutput, $output);
+    }
+}
