d1b78cb2db46b57e138dccd546efefa3b5ca0052
Jacob <jacobproulx@gmail.com>
2023-08-22 10:58:16 -0400

    Added readme

---
+++ README.md
@@ -0,0 +1,35 @@
+# String Service
+
+Helps with latin string manipulation.
+
+### Encode
+
+Encode with `kebabEncode`, `snakeEncode`, `camelEncode` methods. 
+
+Examples :
+
+```php
+use gcob\StringService\StringService;
+
+$input = "  Ma  -chaine##!   PÀS ENcodé     ";
+
+$kebab = StringService::getInstance()->kebabEncode($input);
+echo $kebab; // ma-chaine-pas-encode
+
+$snake = StringService::getInstance()->snakeEncode($input);
+echo $snake; // ma_chaine_pas_encode
+
+$camel = StringService::getInstance()->camelEncode($input);
+echo $camel; // maChainePasEncode
+```
+
+### Remove accents
+
+```php
+use gcob\StringService\StringService;
+
+$input = "déjà vue";
+
+$noAccents = StringService::getInstance()->removeAccents($input);
+echo $noAccents; // deja vue
+```
--- src/StringService.php
+++ src/StringService.php
@@ -9,6 +9,8 @@ class StringService
 
     private static $instance;
 
+    private ?array $accents = null;
+
     private function __construct()
     {
     }
@@ -72,25 +74,27 @@ class StringService
         return $output;
     }
 
-    function removeAccents(string $input): string
+    public function removeAccents(string $input): string
     {
-        $accents = array(
-            'à' => 'a', 'â' => 'a', 'ä' => 'a', 'á' => 'a', 'ã' => 'a',
-            'é' => 'e', 'è' => 'e', 'ê' => 'e', 'ë' => 'e',
-            'î' => 'i', 'ï' => 'i', 'í' => 'i',
-            'ö' => 'o', 'ô' => 'o', 'ò' => 'o', 'ó' => 'o', 'õ' => 'o',
-            'ù' => 'u', 'û' => 'u', 'ü' => 'u', 'ú' => 'u',
-            'ç' => 'c', 'ñ' => 'n',
+        if ($this->accents == null) {
+            $this->accents = [
+                'à' => 'a', 'â' => 'a', 'ä' => 'a', 'á' => 'a', 'ã' => 'a',
+                'é' => 'e', 'è' => 'e', 'ê' => 'e', 'ë' => 'e',
+                'î' => 'i', 'ï' => 'i', 'í' => 'i',
+                'ö' => 'o', 'ô' => 'o', 'ò' => 'o', 'ó' => 'o', 'õ' => 'o',
+                'ù' => 'u', 'û' => 'u', 'ü' => 'u', 'ú' => 'u',
+                'ç' => 'c', 'ñ' => 'n',
 
-            'À' => 'A', 'Â' => 'A', 'Ä' => 'A', 'Á' => 'A', 'Ã' => 'A',
-            'É' => 'E', 'È' => 'E', 'Ê' => 'E', 'Ë' => 'E',
-            'Î' => 'I', 'Ï' => 'I', 'Í' => 'I',
-            'Ö' => 'O', 'Ô' => 'O', 'Ò' => 'O', 'Ó' => 'O', 'Õ' => 'O',
-            'Ù' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ú' => 'U',
-            'Ç' => 'C', 'Ñ' => 'N'
-        );
+                'À' => 'A', 'Â' => 'A', 'Ä' => 'A', 'Á' => 'A', 'Ã' => 'A',
+                'É' => 'E', 'È' => 'E', 'Ê' => 'E', 'Ë' => 'E',
+                'Î' => 'I', 'Ï' => 'I', 'Í' => 'I',
+                'Ö' => 'O', 'Ô' => 'O', 'Ò' => 'O', 'Ó' => 'O', 'Õ' => 'O',
+                'Ù' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ú' => 'U',
+                'Ç' => 'C', 'Ñ' => 'N'
+            ];
+        }
 
-        return strtr($input, $accents);
+        return strtr($input, $this->accents);
     }
 
 }
